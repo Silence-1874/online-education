f1cfd20a55eb6f628209bf1e903ea4c729eff118
Silence <silence1874@foxmail.com>
2023-03-15 16:56:54 +0800

    chore(parent): Spring Initialize

---
+++ online-education-parent/src/main/java/com/silence/OnlineEducationParentApplication.java
@@ -0,0 +1,13 @@
+package com.silence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEducationParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineEducationParentApplication.class, args);
+    }
+
+}
