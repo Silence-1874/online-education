faeffbf2b4fb5628894156a1fa8628dcabca3a24
Silence <silence1874@foxmail.com>
2023-03-16 19:21:18 +0800

    chore(content): Initialize module

---
+++ online-education-content/online-education-content-model/src/main/java/com/silence/OnlineEducationContentModelApplication.java
@@ -0,0 +1,13 @@
+package com.silence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEducationContentModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineEducationContentModelApplication.class, args);
+    }
+
+}
