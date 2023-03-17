91e30793f1521524a22b42dfed8a9e8fab933824
Silence <silence1874@foxmail.com>
2023-03-17 15:09:11 +0800

    chore(generator): Generate content

---
+++ online-education-generator/src/main/java/com/silence/content/mapper/TeachplanMapper.java
@@ -0,0 +1,16 @@
+package com.silence.content.mapper;
+
+import com.silence.content.model.po.Teachplan;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ * 课程计划 Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface TeachplanMapper extends BaseMapper<Teachplan> {
+
+}
---
+++ online-education-generator/src/main/java/com/silence/content/service/impl/CourseMarketServiceImpl.java
@@ -0,0 +1,23 @@
+package com.silence.content.service.impl;
+
+import com.silence.content.model.po.CourseMarket;
+import com.silence.content.mapper.CourseMarketMapper;
+import com.silence.content.service.CourseMarketService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * <p>
+ * 课程营销信息 服务实现类
+ * </p>
+ *
+ * @author itcast
+ */
+@Slf4j
+@Service
+public class CourseMarketServiceImpl extends ServiceImpl<CourseMarketMapper, CourseMarket> implements CourseMarketService {
+
+}
