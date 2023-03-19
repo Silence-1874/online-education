aac85779bada84947c95d23d74e26eeb1217e1f9
Silence <silence1874@foxmail.com>
2023-03-19 19:42:53 +0800

    doc(content): Add Swagger

--- online-education-base/src/main/java/com/silence/base/model/PageParams.java
+++ online-education-base/src/main/java/com/silence/base/model/PageParams.java
@@ -1,5 +1,6 @@
 package com.silence.base.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -12,10 +13,10 @@ import lombok.Data;
 @AllArgsConstructor
 public class PageParams {
 
-    // 当前页码
+    @ApiModelProperty("页码")
     private Long pageNo = 1L;
 
-    // 每页记录数
+    @ApiModelProperty("每页记录数")
     private Long pageSize = 10L;
 
 }
--- online-education-content/online-education-content-api/src/main/java/com/silence/content/api/CourseBaseInfoController.java
+++ online-education-content/online-education-content-api/src/main/java/com/silence/content/api/CourseBaseInfoController.java
@@ -4,6 +4,8 @@ import com.silence.base.model.PageParams;
 import com.silence.base.model.PageResult;
 import com.silence.model.dto.QueryCourseParamsDTO;
 import com.silence.model.po.CourseBase;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description 课程查询接口
  * @Date 2023/3/19
  */
+@Api(value = "课程信息编辑接口",tags = "课程信息编辑接口")
 @RestController
 public class CourseBaseInfoController {
 
+    @ApiOperation("课程查询接口")
     @PostMapping(value = "/course/list")
     public PageResult<CourseBase> list(PageParams pageParams,
                                        @RequestBody(required = false) QueryCourseParamsDTO queryCourseParams) {
