f71d39bec5e5b7764b5dfd3093a48a8c933ce4b7
Silence <silence1874@foxmail.com>
2023-03-21 16:07:39 +0800

    chore(system): Import module

---
+++ online-education-system/online-education-system-service/src/main/java/com/silence/system/mapper/DictionaryMapper.java
@@ -0,0 +1,15 @@
+package com.silence.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.silence.system.model.po.Dictionary;
+
+/**
+ * <p>
+ * 数据字典 Mapper 接口
+ * </p>
+ *
+ * @author silence
+ */
+public interface DictionaryMapper extends BaseMapper<Dictionary> {
+
+}
