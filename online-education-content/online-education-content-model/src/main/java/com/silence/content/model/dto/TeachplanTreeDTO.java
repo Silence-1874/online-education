962afd3409a61f2391ea0f13efedd9888b8309f6
Silence <silence1874@foxmail.com>
2023-03-29 21:39:07 +0800

    feat(content): Add mapper Teachplan-selectTreeNodes

---
+++ online-education-content/online-education-content-model/src/main/java/com/silence/content/model/dto/TeachplanTreeDTO.java
@@ -0,0 +1,26 @@
+package com.silence.content.model.dto;
+
+import com.silence.content.model.po.Teachplan;
+import com.silence.content.model.po.TeachplanMedia;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @Author silence
+ * @Description 教学计划树节点
+ * @Date 2023/3/28
+ */
+@Data
+@ApiModel(value = "TeachplanDTO", description = "教学计划")
+public class TeachplanTreeDTO extends Teachplan {
+
+    @ApiModelProperty("课程关联的媒资信息")
+    TeachplanMedia teachplanMedia;
+
+    @ApiModelProperty("子节点")
+    List<TeachplanTreeDTO> childrenTreeNodes;
+
+}
--- online-education-content/online-education-content-service/src/main/java/com/silence/content/mapper/TeachplanMapper.java
+++ online-education-content/online-education-content-service/src/main/java/com/silence/content/mapper/TeachplanMapper.java
@@ -1,7 +1,10 @@
 package com.silence.content.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.silence.content.model.dto.TeachplanTreeDTO;
 import com.silence.content.model.po.Teachplan;
+
+import java.util.List;
 
 /**
  * <p>
@@ -11,5 +14,12 @@ import com.silence.content.model.po.Teachplan;
  * @author silence
  */
 public interface TeachplanMapper extends BaseMapper<Teachplan> {
+
+    /**
+     * @Author silence
+     * @Description 查询教学计划树
+     * @Date 2023/3/28
+     */
+    List<TeachplanTreeDTO> selectTreeNodes(Long courseId);
 
 }
