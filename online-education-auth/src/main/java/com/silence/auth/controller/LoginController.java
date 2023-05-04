63f25f4731c0d157a0589700930a2875d831471f
Silence <silence1874@foxmail.com>
2023-05-04 11:42:50 +0800

    feat(auth): Import Spring Security

--- online-education-auth/src/main/java/com/silence/auth/controller/LoginController.java
+++ online-education-auth/src/main/java/com/silence/auth/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.silence.ucenter.mapper.OeUserMapper;
 import com.silence.ucenter.model.po.OeUser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,11 +33,13 @@ public class LoginController {
     }
 
     @RequestMapping("/r/r1")
+    @PreAuthorize("hasAuthority('p1')")
     public String r1() {
         return "访问r1资源";
     }
 
     @RequestMapping("/r/r2")
+    @PreAuthorize("hasAuthority('p2')")
     public String r2() {
         return "访问r2资源";
     }
