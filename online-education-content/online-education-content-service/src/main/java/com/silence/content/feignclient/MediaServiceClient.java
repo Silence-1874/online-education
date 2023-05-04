ae8ad78a22ea8c6c58ffc4a45945079c80487f26
Silence <silence1874@foxmail.com>
2023-05-04 09:03:17 +0800

    feat(content): Fix http method

--- online-education-content/online-education-content-service/src/main/java/com/silence/content/feignclient/MediaServiceClient.java
+++ online-education-content/online-education-content-service/src/main/java/com/silence/content/feignclient/MediaServiceClient.java
@@ -3,7 +3,7 @@ package com.silence.content.feignclient;
 import com.silence.content.config.MultipartSupportConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,7 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 @FeignClient(value = "media-api",configuration = MultipartSupportConfig.class)
 public interface MediaServiceClient {
 
-    @RequestMapping(value = "/media/upload/coursefile",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/media/upload/coursefile",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     String uploadFile(@RequestPart("filedata") MultipartFile upload, @RequestParam(value = "objectName",required=false) String objectName);
 
 }
